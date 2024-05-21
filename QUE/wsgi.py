708caf1ba80cdb0dee11ab36752e752850546623
que.ceo <que.ceo24@gmail.com>
2024-05-21 03:44:59 +0200

    WSGI Edit

--- QUE/wsgi.py
+++ QUE/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/queapp/QUE')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QUE.settings')
 
