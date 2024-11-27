1acb06d70c6162ce6d718284f67673f34680d125
Que_ta <167036850+queapp24@users.noreply.github.com>
2024-11-28 00:57:32 +0100

    Missed the /

--- Board/views.py
+++ Board/views.py
@@ -184,7 +184,7 @@ def Que_Main(URL):
 @login_required
 def Host_View(request):
     settings = Setting.load()
-    URL = 'home/queapp/QUE/testn.html'
+    URL = '/home/queapp/QUE/testn.html'
     RATE = settings.S_RATE
 
     Que_Main(URL)
